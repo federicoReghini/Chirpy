53b3e7c5ee269036249b1933bfb715f5ee99f9f3
federicoReghini <federicoreghini@gmail.com>
2025-08-18 20:38:33 +0200

    feat: add metrics middleware and reset handler
    Introduce a middleware to track file server hits and expose metrics
    via the `/metrics` endpoint. Added a `/reset` handler to reset the
    hit counter. Updated route setup to ensure proper middleware application
    and added a redirect for `/app` to `/app/`.

---
+++ http_handler.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func health(w http.ResponseWriter, req *http.Request) {
+	req.Header.Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
+}
--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (c *apiConfig) printRequestesNumber(w http.ResponseWriter, req *http.Request) {
+	req.Header.Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", c.fileServerHits.Load())
+}
 
 func main() {
 	serveMux := http.NewServeMux()
-	server := http.Server{
-		Addr:    ":8080",
+	const port = "8080"
+	const filepathRoot = "."
+
+	apiCfg := apiConfig{
+		fileServerHits: atomic.Int32{},
+	}
+
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	serveMux.HandleFunc("GET /healthz", health)
+	serveMux.HandleFunc("GET /metrics", apiCfg.printRequestesNumber)
+	serveMux.HandleFunc("POST /reset", apiCfg.handlerReset)
+
+	server := &http.Server{
+		Addr:    ":" + port,
 		Handler: serveMux,
 	}
 
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+
+	log.Fatal(server.ListenAndServe())
 }
