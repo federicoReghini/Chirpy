c165c09e37108ef7cb5bb78b69acea3c2145888b
federicoReghini <federicoreghini@gmail.com>
2025-08-19 08:22:10 +0200

    feat(api): improve handler functions and routing
    - Renamed `health` to `handlerHealth` for consistency
    - Updated `printRequestesNumber` to `handlerMetrics` with HTML response
    - Added `createApiPath` utility for dynamic route creation
    - Fixed routing prefixes for `/api/`, `/app/`, and `/admin/`
    - Implemented missing `health` and `handlerReset` functions
    - Corrected POST route formatting in `createApiPath`

--- http_handler.go
+++ http_handler.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func health(w http.ResponseWriter, req *http.Request) {
+func hanlderHealth(w http.ResponseWriter, req *http.Request) {
 	req.Header.Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
--- main.go
+++ main.go
@@ -18,25 +18,40 @@ func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
-func (c *apiConfig) printRequestesNumber(w http.ResponseWriter, req *http.Request) {
-	req.Header.Set("Content-Type", "text/plain; charset=utf-8")
+func (c *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	req.Header.Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hits: %d", c.fileServerHits.Load())
+
+	template := `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`
+
+	fmt.Fprintf(w, template, c.fileServerHits.Load())
+}
+
+func createApiPath(method, prefix, path string) string {
+	return fmt.Sprintf("%s %s%s", method, prefix, path)
 }
 
 func main() {
 	serveMux := http.NewServeMux()
 	const port = "8080"
 	const filepathRoot = "."
+	const apiPrefix = "/api/"
+	const appPrefix = "/app/"
+	const adminPrefix = "/admin/"
 
 	apiCfg := apiConfig{
 		fileServerHits: atomic.Int32{},
 	}
 
-	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	serveMux.HandleFunc("GET /healthz", health)
-	serveMux.HandleFunc("GET /metrics", apiCfg.printRequestesNumber)
-	serveMux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	serveMux.Handle(appPrefix, apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	serveMux.HandleFunc(createApiPath("GET", apiPrefix, "healthz"), hanlderHealth)
+	serveMux.HandleFunc(createApiPath("GET", adminPrefix, "metrics"), apiCfg.handlerMetrics)
+	serveMux.HandleFunc(createApiPath("POST ", adminPrefix, "reset"), apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
